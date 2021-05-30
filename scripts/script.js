7299c743189eeb20b8eb75b84f6ce632df66e419
RainyKnight <stoltmanlucas@gmail.com>
2021-05-30 10:53:15 -0700

    Bind number buttons to relevant functions

--- scripts/script.js
+++ scripts/script.js
@@ -4,7 +4,58 @@ function print() {
     document.getElementById("output").innerHTML = output;
 }
 
+function clear() {
+    output = null;
+    print();
+}
+
+function zero() {
+    output = 0;
+    print();
+}
+
+
 function one() {
-    newOutput = 1;
+    output = 1;
+    print();
+}
+
+function two() {
+    output = 2;
+    print();
+}
+
+function three() {
+    output = 3;
+    print();
+}
+
+function four() {
+    output = 4;
+    print();
+}
+
+function five() {
+    output = 5;
+    print();
+}
+
+function six() {
+    output = 6;
+    print();
+}
+
+function seven() {
+    output = 7;
+    print();
+}
+
+function eight() {
+    output = 8;
+    print();
+}
+
+function nine() {
+    output = 9;
     print();
 }
